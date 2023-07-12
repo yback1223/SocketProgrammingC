4da915b2818a8bf9d7868157f57b30cf20f8ed5a
yback1223 <yoyob1223@gmail.com>
2023-07-13 04:57:15 +0900

    Add socketPair

---
+++ socketpair/socketPairTwoProcess.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <sys/socket.h>
+
+char	data1[ ] = "From Parent";
+char	data2[ ] = "From Child";
+
+main( ) {
+	int	pid;
+	int	sd[2], result;
+	char	buf[BUFSIZ];
+
+	result = socketpair(PF_LOCAL, SOCK_STREAM, 0, sd);
+
+	pid = fork( );
+	if(pid > 0) {  // 부모 프로세스
+		close(sd[1]);
+
+		read(sd[0], buf, BUFSIZ);
+		printf("[parent:%d] %s\n", getpid( ), buf);
+
+		write(sd[0], data1, sizeof(data1));
+
+		close(sd[0]);
+	} else if(pid == 0) { // 자식 프로세스
+		close(sd[0]);
+
+		write(sd[1], data2, sizeof(data2));
+
+		read(sd[1], buf, BUFSIZ);
+		printf("[child:%d] %s\n", getpid( ), buf);
+
+		close(sd[1]);
+	} else if(pid == -1) {
+		perror("fork( )");
+	}
+}
